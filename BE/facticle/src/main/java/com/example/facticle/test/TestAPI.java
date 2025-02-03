4d697213a9dcfd1d3b4b67d9713dd9f49b27b9c4
Namwoosung <namws0023@gmail.com>
2025-02-03 20:38:16 +0900

    Add config

--- BE/facticle/src/main/java/com/example/facticle/test/TestAPI.java
+++ BE/facticle/src/main/java/com/example/facticle/test/TestAPI.java
@@ -1,7 +1,7 @@
 package com.example.facticle.test;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestAPI {
@@ -9,4 +9,29 @@ public class TestAPI {
     public String testApi(){
         return "ok";
     }
+
+    @GetMapping("/requestParam")
+    public String requestParam(
+            @RequestParam(required = false) String username,
+            @RequestParam(defaultValue = "10") Integer age,
+            //or
+            @ModelAttribute HelloData helloData
+    ){
+        return "For requestParam";
+    }
+
+    @PostMapping("/RequestBody")
+    public String requestBody(
+            //request의 body에 있는 내용을 조회 가능
+            @RequestBody String messageBody,
+            @RequestBody HelloData hellodata //요청시 content-type이 applciation/json이어야 가능
+    ){
+        return "For request body";
+    }
+}
+
+@Data
+class HelloData{
+    private String username;
+    private int age;
 }
