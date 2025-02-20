68de0460f977fc9a515ff9de551325886c6c6c1a
Yang Shin Hee <wlq94814@naver.com>
2025-02-20 20:37:02 +0900

    Chore: temporarily disable StrictMode during development

--- FE/facticle/src/main.tsx
+++ FE/facticle/src/main.tsx
@@ -3,13 +3,50 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
 import { AuthProvider } from './context/index.tsx'
+import { createGlobalStyle } from 'styled-components'
+import NanumGothic from './assets/fonts/NanumBarunGothic.ttf'
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+      font-family: 'Nanum';
+      src: url(${NanumGothic}) format('truetype');
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 ;
+    text-align: center;
+    font-family: 'Nanum';
+  }
+
+  button {
+    cursor: pointer;
+
+    &:hover {
+      outline: none;
+    }
+    &:focus {
+      outline: none;
+    }
+    &:active {
+      outline: none;
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  //<StrictMode>
+  <Router>
     <AuthProvider>
-      <Router>
-        <App />
-      </Router>
+      <GlobalStyles />
+      <App />
     </AuthProvider>
-  </StrictMode>,
+  </Router>
+  //</StrictMode>,
 )
