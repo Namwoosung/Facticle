b3ecc9762165a7c880826f36836ccadd625d93a6
Yang Shin Hee <wlq94814@naver.com>
2025-02-20 23:21:22 +0900

    Add Layouts and Update Routes structure

--- FE/facticle/src/App.tsx
+++ FE/facticle/src/App.tsx
@@ -1,54 +1,28 @@
-import styled from 'styled-components'
 import Home from './pages/home';
 import Login from './pages/auth/login';
 import Register from './pages/auth/register';
 import { Routes, Route } from 'react-router-dom';
 import Redirection from './pages/auth/redirection';
 import RegisterOauth from './pages/auth/register-oauth';
-
-const RootElement = styled.div`
-  @font-face {
-    font-family: 'Nanum';
-    src: url('/assets/fonts/NanumBarunGothic.ttf');
-  }
-
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0 ;
-  text-align: center;
-  font-family: 'Nanum';
-
-  button {
-    cursor: pointer;
-
-    &:hover {
-      outline: none;
-    }
-    &:focus {
-      outline: none;
-    }
-    &:active {
-      outline: none;
-    }
-  }
-
-  a {
-    text-decoration: none;
-  }
-`;
+import MainLayout from './layouts/mainLayout';
+import AuthLayout from './layouts/authLayout';
 
 function App() {
   return (
-    <RootElement>
+    <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/register-oauth" element={<RegisterOauth />} />
-        <Route path='/oauth/callback/*' element={<Redirection />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register-oauth" element={<RegisterOauth />} />
+          <Route path='/oauth/callback/*' element={<Redirection />} />
+        </Route>
       </Routes>
-    </RootElement>
+    </>
   )
 }
 
