3a19c0ee420e2480edfac94ab34070e85e4430d9
Yang Shin Hee <wlq94814@naver.com>
2025-04-30 16:00:32 +0900

    Fix a dialog style

--- FE/facticle/src/components/dialog/dialog.styles.tsx
+++ FE/facticle/src/components/dialog/dialog.styles.tsx
@@ -6,7 +6,7 @@ export const DialogOverlay = styled.div<{ open: boolean }>`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.3);
   display: ${({ open }) => (open ? 'flex' : 'none')};
   justify-content: center;
   align-items: center;
